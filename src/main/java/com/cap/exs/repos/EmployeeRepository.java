20580fc12766440aa78a41ab097980e66bfc8e6c
Naman-Mtal <namanmittal153@gmail.com>
2021-02-27 18:41:26 +0530

    Employee REpository added

--- src/main/java/com/cap/exs/repos/EmployeeRepository.java
+++ src/main/java/com/cap/exs/repos/EmployeeRepository.java
@@ -1,5 +1,12 @@
 package com.cap.exs.repos;
 
-public class EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.cap.exs.entities.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+
+	
 }
