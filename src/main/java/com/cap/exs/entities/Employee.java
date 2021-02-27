b9f8be3f58fd63f9ea6d1574df4204010c3e2518
Naman-Mtal <namanmittal153@gmail.com>
2021-02-27 18:37:50 +0530

    Employee entity updated

--- src/main/java/com/cap/exs/entities/Employee.java
+++ src/main/java/com/cap/exs/entities/Employee.java
@@ -1,5 +1,137 @@
 package com.cap.exs.entities;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Employee {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int empId;
+	private String empName;
+	private String empPAN;
+	private String empDesignation;
+	private String empDomain;
+	private String empDOJ;
+	private String empDOB;
+	private String empSalary;
+	private String empEmailId;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private LoginDetails loginDetails;
+
+	
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(String empName, String empPAN, String empDesignation, String empDomain, String empDOJ,
+			String empDOB, String empSalary, String empEmailId, LoginDetails loginDetails) {
+		super();
+		this.empName = empName;
+		this.empPAN = empPAN;
+		this.empDesignation = empDesignation;
+		this.empDomain = empDomain;
+		this.empDOJ = empDOJ;
+		this.empDOB = empDOB;
+		this.empSalary = empSalary;
+		this.empEmailId = empEmailId;
+		this.loginDetails = loginDetails;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getEmpPAN() {
+		return empPAN;
+	}
+
+	public void setEmpPAN(String empPAN) {
+		this.empPAN = empPAN;
+	}
+
+	public String getEmpDesignation() {
+		return empDesignation;
+	}
+
+	public void setEmpDesignation(String empDesignation) {
+		this.empDesignation = empDesignation;
+	}
+
+	public String getEmpDomain() {
+		return empDomain;
+	}
+
+	public void setEmpDomain(String empDomain) {
+		this.empDomain = empDomain;
+	}
+
+	public String getEmpDOJ() {
+		return empDOJ;
+	}
+
+	public void setEmpDOJ(String empDOJ) {
+		this.empDOJ = empDOJ;
+	}
+
+	public String getEmpDOB() {
+		return empDOB;
+	}
+
+	public void setEmpDOB(String empDOB) {
+		this.empDOB = empDOB;
+	}
+
+	public String getEmpSalary() {
+		return empSalary;
+	}
+
+	public void setEmpSalary(String empSalary) {
+		this.empSalary = empSalary;
+	}
+
+	public String getEmpEmailId() {
+		return empEmailId;
+	}
+
+	public void setEmpEmailId(String empEmailId) {
+		this.empEmailId = empEmailId;
+	}
+
+	public LoginDetails getLoginDetails() {
+		return loginDetails;
+	}
+
+	public void setLoginDetails(LoginDetails loginDetails) {
+		this.loginDetails = loginDetails;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empPAN=" + empPAN + ", empDesignation="
+				+ empDesignation + ", empDomain=" + empDomain + ", empDOJ=" + empDOJ + ", empDOB=" + empDOB
+				+ ", empSalary=" + empSalary + ", empEmailId=" + empEmailId + ", loginDetails=" + loginDetails + "]";
+	}
+	
+	
+	
+	
 }
