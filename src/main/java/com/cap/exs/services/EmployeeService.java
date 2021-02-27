6733fae4366c1642c3f39be0c30f52ea6acd71ec
Naman-Mtal <namanmittal153@gmail.com>
2021-02-27 18:58:55 +0530

    EmployeeService class added

--- src/main/java/com/cap/exs/services/EmployeeService.java
+++ src/main/java/com/cap/exs/services/EmployeeService.java
@@ -1,5 +1,75 @@
 package com.cap.exs.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cap.exs.entities.Employee;
+import com.cap.exs.repos.EmployeeRepository;
+
+@Service
 public class EmployeeService {
 
+	@Autowired
+	EmployeeRepository employeeRepository;
+	
+	public Employee addEmployee(Employee employee) {
+		
+		return employeeRepository.save(employee);
+	}
+	
+	public List<Employee> getEmployees(){
+		
+		List<Employee> employees = employeeRepository.findAll();
+		
+		if(employees.isEmpty())
+		{
+			//throw exception
+		}
+		
+		return employees;
+		
+	}
+	
+	public Employee findByEmployeeCode(int empId) {
+		
+		Optional<Employee> employee = employeeRepository.findById(empId);
+		
+		if(!employee.isPresent())
+		{
+			//throw exception
+		}
+		
+		return employee.get();
+		
+	}
+	
+	public void deleteEmpById(int empId) {
+		
+		Employee employee = this.findByEmployeeCode(empId);
+		
+		employeeRepository.delete(employee);
+		
+	}
+	
+	public Employee updateEmployee(Employee employee) {
+		
+		//update logic
+		return null;
+	}
+	
+	public Employee getDetailsByAll(String username, String password, String role) {
+		
+		Employee employee = employeeRepository.findByUsernameAndPaswordAndRole(username, password, role);
+		
+		if(employee==null)
+		{
+			//throw exception
+		}
+		
+		return employee;
+	}
+	
 }
