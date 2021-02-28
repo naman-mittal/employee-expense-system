e18d767944e455a9757e803dd31d80a26f617f70
ashutoshup99 <ashuup99@gmail.com>
2021-02-28 12:47:43 +0530

    ExpenseClaimRepository updated

--- src/main/java/com/cap/exs/repos/ExpenseClaimRepository.java
+++ src/main/java/com/cap/exs/repos/ExpenseClaimRepository.java
@@ -2,20 +2,11 @@ package com.cap.exs.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.cap.exs.entities.Employee;
+
 
 public interface ExpenseClaimRepository extends JpaRepository<ExpenseClaim, Integer> {
-	public List<ExpenseClaim> getAllExpenseClaim();
 	
-	public ExpenseClaim addExpenseClaim(ExpenseClaim expenseClaim);
-	
-	public ExpenseClaim findExpenseClaimById(int expenseCodeID);
-	
-	public ExpenseClaim uppdateExpenseClaim(ExpenseClaim expenseClaim);
-	
-	public ExpenseClaim deleteExpenseClaimById(int id);
-	
-	public List<ExpenseClaim> getAllClaimsByEmployee(Employee emp);
-
-
+	public Employee findByEmployee(Employee employee);
 
 }
