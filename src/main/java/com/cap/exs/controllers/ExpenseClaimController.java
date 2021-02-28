bd4703e42003bd3ba2104dbe62372127a43ea730
unknown <harshbansal2371998@gmail.com>
2021-02-28 16:02:01 +0530

    add method in expenseController class

--- src/main/java/com/cap/exs/controllers/ExpenseClaimController.java
+++ src/main/java/com/cap/exs/controllers/ExpenseClaimController.java
@@ -1,8 +1,14 @@
 package com.cap.exs.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cap.exs.entities.ExpenseClaim;
 import com.cap.exs.services.EmployeeService;
 import com.cap.exs.services.ExpenseClaimService;
 
@@ -10,9 +16,12 @@ import com.cap.exs.services.ExpenseClaimService;
 public class ExpenseClaimController {
 	
 	@Autowired
-	EmployeeService employeeService;
-	
-	@Autowired
 	ExpenseClaimService expenseClaimService;
+	
+	@GetMapping("/expensesClaim")
+	@ResponseStatus(code = HttpStatus.FOUND)
+	public List<ExpenseClaim> getAllExpenseClaim(){
+		return expenseClaimService.getAllExpenseClaim();
+	}
 
 }
