887851a1fbf97b06946dadc749bb87bda4a52a2e
Ishan Agrawal <ishan38@outlook.com>
2021-03-08 09:27:51 +0530

    Project controller added

--- src/main/java/com/cap/exs/repos/IProjectRepository.java
+++ src/main/java/com/cap/exs/repos/IProjectRepository.java
@@ -1,11 +1,12 @@
 package com.cap.exs.repos;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.cap.exs.entities.*; 
+import com.cap.exs.entities.Project; 
 
 @Repository
-public interface IProjectRepository {
+public interface IProjectRepository extends JpaRepository<Project, Integer>{
 	
 	public Project findByCode(Project project);
 	
--- src/main/java/com/cap/exs/services/ProjectService.java
+++ src/main/java/com/cap/exs/services/ProjectService.java
@@ -6,24 +6,43 @@ import org.springframework.stereotype.Service;
 
 import com.cap.exs.entities.Project;
 import com.cap.exs.repos.*;
+import com.cap.exs.service_interfaces.IProjectService;
 
 
 @Service 
-public interface ProjectService {
+public class ProjectService implements IProjectService{
 
-	public List<Project> getAllProject();
+	public List<Project> getAllProject(){
+		return null;
+		
+	}
 	
-	public Project addProject(Project project);
+	public Project addProject(Project project) {
+		return project;
+		
+	}
 	
-//	public List<Project> getAllProjectCode(int Id);
+//	public List<Project> getAllProjectCode(int Id){}
 	
-	public Project updateProject(Project project);
+	public Project updateProject(Project project) {
+		return project;
+		
+	}
 	
-	public Project deleteProjectById(int id);
+	public Project deleteProjectById(int id) {
+		return null;
+		
+	}
 	
-//	public void deleteAllProject();
+//	public void deleteAllProject(){}
 	
-	public List<Integer> getAllProjectCodes();
+	public List<Integer> getAllProjectCodes(){
+		return null;
+		
+	}
 	
-	public Project findByCode(int projectCode);
+	public Project findByCode(int projectCode) {
+		return null;
+		
+	}
 }
