4685a1df9af516479167eda79d1d5e9906a873be
Ishan Agrawal <ishan38@outlook.com>
2021-03-09 01:08:07 +0530

    Project module updated

--- src/main/java/com/cap/exs/repos/IProjectRepository.java
+++ src/main/java/com/cap/exs/repos/IProjectRepository.java
@@ -1,5 +1,7 @@
 package com.cap.exs.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,6 @@ import com.cap.exs.entities.Project;
 @Repository
 public interface IProjectRepository extends JpaRepository<Project, Integer>{
 	
-	public Project findByCode(Project project);
+	public List<Integer> getAllProjectCodes();
 	
 }
--- src/main/java/com/cap/exs/services/ProjectService.java
+++ src/main/java/com/cap/exs/services/ProjectService.java
@@ -1,48 +1,77 @@
 package com.cap.exs.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cap.exs.entities.Expense;
 import com.cap.exs.entities.Project;
-import com.cap.exs.repos.*;
+import com.cap.exs.repos.IProjectRepository;
 import com.cap.exs.service_interfaces.IProjectService;
+import com.cap.exs.exceptions.ExpenseNotFoundException;
+import com.cap.exs.exceptions.ProjectNotFoundException;
 
 
 @Service 
 public class ProjectService implements IProjectService{
-
+	
+	@Autowired
+	IProjectRepository projectRepository;
+	
 	public List<Project> getAllProject(){
-		return null;
+		List<Project> projects = new ArrayList<Project>();
+		projects = projectRepository.findAll();
 		
+		if(projects.isEmpty()) {
+			throw new ProjectNotFoundException("No projects found...");
+		}
+		
+		return projects;
 	}
 	
+	
 	public Project addProject(Project project) {
-		return project;
-		
+		return projectRepository.save(project);
 	}
 	
 //	public List<Project> getAllProjectCode(int Id){}
 	
 	public Project updateProject(Project project) {
-		return project;
-		
+		return projectRepository.save(project);
 	}
 	
+	
 	public Project deleteProjectById(int id) {
-		return null;
-		
+		Project project = projectRepository.findById(id).get();
+		projectRepository.delete(project);
+		return project;	
 	}
+	
 	
 //	public void deleteAllProject(){}
 	
+	
 	public List<Integer> getAllProjectCodes(){
-		return null;
+		List<Integer> projectCodes = new ArrayList<Integer>();
+		projectCodes = projectRepository.getAllProjectCodes();
 		
+		if(projectCodes.isEmpty()) {
+			throw new ProjectNotFoundException("No project code found...");
+		}
+		
+		return projectCodes;
 	}
 	
+	
 	public Project findByCode(int projectCode) {
-		return null;
-		
+		Optional<Project> project = projectRepository.findById(projectCode);
+		if(!project.isPresent())
+		{
+			throw new ProjectNotFoundException("No project found with projectCode: " + projectCode);
+		}
+		return project.get();		
 	}
 }
