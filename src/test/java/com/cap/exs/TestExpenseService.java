9b83991a184e7ce3c7c13124d66dd02bc3d76f1c
Amanpratapsing <amanpratapsingh0801@gmail.com>
2021-03-09 21:59:08 +0530

    expense module updated

--- src/test/java/com/cap/exs/TestExpenseService.java
+++ src/test/java/com/cap/exs/TestExpenseService.java
@@ -1,11 +1,16 @@
 package com.cap.exs;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
+import com.cap.exs.entities.Expense;
 import com.cap.exs.repos.IExpenseRepository;
 import com.cap.exs.services.ExpenseService;
 
@@ -23,7 +28,23 @@ public class TestExpenseService {
 	@Test
 	public void testAddExpense()
 	{
-		// 
+		Expense exp = new Expense();
+		exp.setExpenseType("abc");
+		exp.setExpenseDescription("okok");
+		assertEquals(1, expenseRepository.count());
+		
 	}
+	
+	
+	
+	@Test
+	public void testFindByExpenseCode()
+	{
+			Expense exp = expenseService.findByCode(1);
+			assertNotNull(exp);
+			
+	}
+	
+	
 
 }
