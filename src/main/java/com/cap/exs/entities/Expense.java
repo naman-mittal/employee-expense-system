82cefa92767e3c718a556989a28809eb9a5f3a45
Amanpratapsing <amanpratapsingh0801@gmail.com>
2021-03-09 10:16:39 +0530

    updated

--- src/main/java/com/cap/exs/entities/Expense.java
+++ src/main/java/com/cap/exs/entities/Expense.java
@@ -1,7 +1,15 @@
 package com.cap.exs.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Expense {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int expenseCode;
 	private String expenseType;
 	private String expenseDescription;
