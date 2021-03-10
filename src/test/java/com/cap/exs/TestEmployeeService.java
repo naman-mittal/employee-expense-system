07173e39f90706f0ab10b07bca691f3eb3f777d5
Naman-Mtal <namanmittal153@gmail.com>
2021-03-10 16:20:29 +0530

    Test case updated with MockVito

--- src/test/java/com/cap/exs/TestEmployeeService.java
+++ src/test/java/com/cap/exs/TestEmployeeService.java
@@ -1,18 +1,24 @@
 package com.cap.exs;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
 
-import java.util.List;
+import java.util.Arrays;
+import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.cap.exs.entities.Employee;
 import com.cap.exs.entities.LoginDetails;
+import com.cap.exs.exceptions.EmployeeNotFoundException;
+import com.cap.exs.exceptions.UsernameAlreadyExistException;
 import com.cap.exs.repos.IEmployeeRepository;
 import com.cap.exs.services.EmployeeService;
 
@@ -24,10 +30,11 @@ public class TestEmployeeService {
 	@Autowired
 	EmployeeService employeeService;
 
-	@Autowired
+
+	@MockBean
 	IEmployeeRepository employeeRepository;
 	
-	@Test
+	//@Test
 	public void testAddEmployee() {
 		
 		Employee emp = new Employee();
@@ -39,21 +46,22 @@ public class TestEmployeeService {
 		
 		emp.setLoginDetails(ld);
 		
-		employeeService.addEmployee(emp);
+		when(employeeRepository.save(emp)).thenReturn(emp);
 		
-		assertEquals(3, employeeRepository.count());
+		assertEquals(emp, employeeService.addEmployee(emp));
 		
 	}
 
-	//@Test(expected = UsernameAlreadyExistException.class)
+	@Test(expected = UsernameAlreadyExistException.class)
 		public void testAddEmployeeWithExistingUsername() {
 			
 			Employee emp = new Employee();
 			emp.setEmpName("John");
 			
 			LoginDetails ld = new LoginDetails("test2", "test", "tester");
-			
 			emp.setLoginDetails(ld);
+			
+			when(employeeRepository.save(emp)).thenThrow(UsernameAlreadyExistException.class);
 			
 			employeeService.addEmployee(emp);
 			
@@ -72,37 +80,56 @@ public class TestEmployeeService {
 	//@Test
 	public void testFindByEmployeeCode()
 	{
-		Employee emp = employeeService.findByEmployeeCode(1);
-		assertNotNull(emp);
+		Employee emp = new Employee("Naman", null, null, null, null, null, null, null, new LoginDetails());
+		emp.setEmpId(1);
+		when(employeeRepository.findById(1)).thenReturn(Optional.of(emp));
+		assertEquals(emp,employeeService.findByEmployeeCode(2));
 		
 	}
 
-	//@Test(expected = EmployeeNotFoundException.class)
+	@Test(expected = EmployeeNotFoundException.class)
 	public void testFindNonExistingEmployee()
 	{
+		when(employeeRepository.findById(100)).thenThrow(EmployeeNotFoundException.class);
 		Employee emp = employeeService.findByEmployeeCode(100);
-		
+		System.out.println(emp);
 		
 	}
 	
-	//@Test
+	@Test
 	public void testGetEmployees()
 	{
-		List<Employee> employees = employeeService.getEmployees();
-		assertEquals(3, employees.size());
+		
+		Employee emp1 = new Employee();
+		emp1.setEmpName("naman");
+		Employee emp2 = new Employee();
+		emp2.setEmpName("aman");
+		
+		when(employeeRepository.findAll()).thenReturn(Arrays.asList(emp1,emp2));
+		assertEquals(2, employeeService.getEmployees().size());
 	}
 	
-	//@Test
+	@Test
 	public void testDeleteEmployeeById()
 	{
+		Employee emp = new Employee("Naman", null, null, null, null, null, null, null, new LoginDetails());
+		emp.setEmpId(1);
+		when(employeeRepository.findById(1)).thenReturn(Optional.of(emp));
 		employeeService.deleteEmpById(1);
-		assertEquals(2, employeeRepository.count());
+		
+		verify(employeeRepository,times(1)).delete(emp);
+		
 	}
 	
-	//@Test(expected = EmployeeNotFoundException.class)
+	@Test(expected = EmployeeNotFoundException.class)
 	public void testDeleteNonExistingEmployee()
 	{
-		employeeService.deleteEmpById(100);
+		Employee emp = new Employee("Naman", null, null, null, null, null, null, null, new LoginDetails());
+		emp.setEmpId(100);
+		when(employeeRepository.findById(emp.getEmpId())).thenThrow(EmployeeNotFoundException.class);
+		employeeService.deleteEmpById(emp.getEmpId());
+		
+		verify(employeeRepository,times(1)).delete(emp);
 	}
 	
 }
