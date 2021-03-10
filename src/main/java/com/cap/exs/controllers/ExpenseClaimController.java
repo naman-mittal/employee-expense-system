230e3c5fcf3029197f41ab9bec66d37b91a8c374
unknown <harshbansal2371998@gmail.com>
2021-03-10 17:42:49 +0530

    added mockbean in expenseClaim test file

--- src/main/java/com/cap/exs/controllers/ExpenseClaimController.java
+++ src/main/java/com/cap/exs/controllers/ExpenseClaimController.java
@@ -1,5 +1,6 @@
 package com.cap.exs.controllers;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.validation.constraints.Min;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -62,5 +64,11 @@ public class ExpenseClaimController {
 	public List<ExpenseClaim> getAllClaimsByEmployee(Employee emp){
 		return expenseClaimService.getAllClaimsByEmployee(emp);
 	}
+	
+//	@GetMapping("/expenseClaims")
+//	@ResponseStatus(code = HttpStatus.OK)
+//	public List<ExpenseClaim> findAllClaimsBetweenDates(@RequestParam(name = "startDate") LocalDate startDate ,@RequestParam(name = "endDate") LocalDate endDate){
+//		return expenseClaimService.findAllClaimsBetweenDates(startDate, endDate);
+//	}
 
 }
--- src/test/java/com/cap/exs/TestExpenseClaimService.java
+++ src/test/java/com/cap/exs/TestExpenseClaimService.java
@@ -1,25 +1,28 @@
 package com.cap.exs;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.cap.exs.entities.Employee;
 import com.cap.exs.entities.Expense;
 import com.cap.exs.entities.ExpenseClaim;
+import com.cap.exs.entities.LoginDetails;
 import com.cap.exs.entities.Project;
 import com.cap.exs.repos.IExpenseClaimRepository;
-import com.cap.exs.services.EmployeeService;
 import com.cap.exs.services.ExpenseClaimService;
-import com.cap.exs.services.ExpenseService;
-import com.cap.exs.services.ProjectService;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -28,17 +31,8 @@ public class TestExpenseClaimService {
 	@Autowired
 	ExpenseClaimService expenseClaimService;
 	
-	@Autowired
+	@MockBean
 	IExpenseClaimRepository expenseClaimRepository;
-	
-	@Autowired
-	ExpenseService expenseService;
-	
-	@Autowired
-	ProjectService projectService;
-	
-	@Autowired
-	EmployeeService employeeService;
 
 //	@Test
 	public void testAddExpenseClaim() {
@@ -52,32 +46,62 @@ public class TestExpenseClaimService {
 		LocalDate endDate = LocalDate.of(2021, 05, 20);
 		expenseClaim.setEndDate(endDate);
 		
-		Expense expense = new Expense();
-		expense = expenseService.getExpenseByCode(1);
-		expenseClaim.setExpense(expense);
+//		Expense expense = new Expense(1,"abc","okok");
+//		expenseClaim.setExpense(expense);
+//		
+//		Project project = new Project(1, "Website", LocalDate.of(2021, 01, 01), LocalDate.of(2021, 05, 20));
+//		expenseClaim.setProject(project);
+//		
+//		Employee employee = new Employee("Naman", null, null, null, null, null, null, null, new LoginDetails());
+//		employee.setEmpId(1);
+//		expenseClaim.setEmployee(employee);
 		
-		Project project = new Project();
-		project = projectService.findByCode(100);
-		expenseClaim.setProject(project);
-		
-		Employee employee = new Employee();
-		employee = employeeService.findByEmployeeCode(2);
-		expenseClaim.setEmployee(employee);
-		
-		expenseClaimService.addExpenseClaim(expenseClaim);
-		assertEquals(2, expenseClaimRepository.count());	
+		when(expenseClaimRepository.save(expenseClaim)).thenReturn(expenseClaim);
+		assertEquals(expenseClaim, expenseClaimService.addExpenseClaim(expenseClaim));	
 	}
 	
 //	@Test
 	public void testGetAllExpenseClaim() {
-		List<ExpenseClaim> allExpenseClaim = expenseClaimService.getAllExpenseClaim();
-		assertEquals(0, allExpenseClaim.size());	
+		ExpenseClaim expenseClaim1 = new ExpenseClaim();
+		
+		expenseClaim1.setExpenseAmount(1000.0);
+		
+		LocalDate startDate1 = LocalDate.of(2021, 01, 01);
+		expenseClaim1.setStartDate(startDate1);
+		
+		LocalDate endDate1 = LocalDate.of(2021, 05, 20);
+		expenseClaim1.setEndDate(endDate1);
+		
+		ExpenseClaim expenseClaim2 = new ExpenseClaim();
+		
+		expenseClaim2.setExpenseAmount(2000.0);
+		
+		LocalDate startDate = LocalDate.of(2021, 05, 01);
+		expenseClaim2.setStartDate(startDate);
+		
+		LocalDate endDate = LocalDate.of(2021, 01, 20);
+		expenseClaim2.setEndDate(endDate);
+		
+		when(expenseClaimRepository.findAll()).thenReturn(Arrays.asList(expenseClaim1,expenseClaim2));
+		assertEquals(2, expenseClaimService.getAllExpenseClaim().size());	
 	}
 	
 //	@Test
 	public void testFetchExpenseClaimById() {
-		ExpenseClaim expenseClaim = expenseClaimService.fetchExpenseClaimById(1);
-		assertNotNull(expenseClaim);
+		ExpenseClaim expenseClaim = new ExpenseClaim();
+		
+		expenseClaim.setExpenseAmount(1000.0);
+		
+		LocalDate startDate = LocalDate.of(2021, 01, 01);
+		expenseClaim.setStartDate(startDate);
+		
+		LocalDate endDate = LocalDate.of(2021, 05, 20);
+		expenseClaim.setEndDate(endDate);
+		
+		expenseClaim.setExpenseCodeId(1);
+		
+		when(expenseClaimRepository.findById(1)).thenReturn(Optional.of(expenseClaim));
+		assertEquals(expenseClaim,expenseClaimService.fetchExpenseClaimById(1));
 	}
 	
 //	@Test
@@ -88,8 +112,22 @@ public class TestExpenseClaimService {
 	
 //	@Test
 	public void testDeleteExpenseClaimById() {
+		ExpenseClaim expenseClaim = new ExpenseClaim();
+		
+		expenseClaim.setExpenseAmount(1000.0);
+		
+		LocalDate startDate = LocalDate.of(2021, 01, 01);
+		expenseClaim.setStartDate(startDate);
+		
+		LocalDate endDate = LocalDate.of(2021, 05, 20);
+		expenseClaim.setEndDate(endDate);
+		
+		expenseClaim.setExpenseCodeId(1);
+		
+		when(expenseClaimRepository.findById(1)).thenReturn(Optional.of(expenseClaim));
 		expenseClaimService.deleteExpenseClaimById(1);
-		assertEquals(1, expenseClaimRepository.count());
+		
+		verify(expenseClaimRepository,times(1)).delete(expenseClaim);
 	}
 	
 //	@Test
