396c176e26ddcb46d7d3a8856eb1d79e92afc5af
Himanshu Nagar <hnagar1305@gmail.com>
2021-03-11 13:44:50 +0530

    testAddDetails, testAddDetailsWithExistingUsername, testDeleteLoginDetailsById (made these 3 test cases)

--- src/test/java/com/cap/exs/TestLoginService.java
+++ src/test/java/com/cap/exs/TestLoginService.java
@@ -1,6 +1,8 @@
 package com.cap.exs;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.Test;
@@ -10,7 +12,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 import com.cap.exs.entities.LoginDetails;
+import com.cap.exs.exceptions.UsernameAlreadyExistException;
 import com.cap.exs.repos.ILoginRepository;
 import com.cap.exs.services.LoginService;
 
@@ -25,7 +29,7 @@ public class TestLoginService {
 	ILoginRepository loginRepository;
 	
 	
-	@Test
+//	@Test
 	public void testAddDetails() {
 	LoginDetails ld =new LoginDetails();
 	ld.setUserName("aman");
@@ -34,10 +38,24 @@ public class TestLoginService {
 
 	when (loginRepository.save(ld)).thenReturn(ld);
 	
-	assertEquals(ld,loginService.addDetails(ld));
-	
-	
-	
+	assertEquals(ld,loginService.addDetails(ld));	
 	}
 	
+//	@Test(expected = UsernameAlreadyExistException.class)
+	public void testAddDetailsWithExistingUsername() {
+		LoginDetails ld = new LoginDetails();
+		ld.setUserName("lalit");
+		when(loginRepository.save(ld)).thenThrow(UsernameAlreadyExistException.class);
+		loginService.addDetails(ld);
+	}
+	@Test
+	public void testDeleteLoginDetailsById()
+	{
+		LoginDetails ld = new LoginDetails();
+		ld.setId(2);
+		when(loginRepository.findById(2)).thenReturn(ld);
+		loginService.deleteDetailsById(2);
+		
+		verify(loginRepository,times(1)).delete(ld);
+	}
 }
