53cbdfd40a0c02ccf8fe88cf96a2cd2a52dd53d2
Naman-Mtal <namanmittal153@gmail.com>
2021-03-11 19:47:12 +0530

    API documented

--- src/main/java/com/cap/exs/controllers/EmployeeController.java
+++ src/main/java/com/cap/exs/controllers/EmployeeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +23,12 @@ import com.cap.exs.entities.Employee;
 import com.cap.exs.request.UpdateEmployeeRequest;
 import com.cap.exs.services.EmployeeService;
 
+import io.swagger.annotations.Api;
+
 @RestController
 @Validated
+@RequestMapping("/api/v1")
+@Api(value = "Employee", tags = { "EmployeeAPI" })
 public class EmployeeController {
 
 	@Autowired
